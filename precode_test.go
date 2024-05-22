dcef2b1eeb2e6d237ee093f7330012fdace8e845
Vladislav <bykasovv@mail.ru>
2024-05-22 23:14:23 +0500

    second iteration

--- precode_test.go
+++ precode_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,28 +11,25 @@ import (
 )
 
 func TestMainHandleWhenRequestIsIncorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	require.Equal(t, responseRecorder.Code, http.StatusOK)
-
-	body, _ := ioutil.ReadAll(req.Body)
-	require.NotEmpty(t, body)
+	require.NotEmpty(t, responseRecorder.Body.String())
 }
 
 func TestMainHandleWhenCityIsIncorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=city", nil)
-	city := req.URL.Query().Get("city")
+	req := httptest.NewRequest("GET", "/cafe?count=3&city=krasnodar", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.NotEqual(t, responseRecorder.Code, http.StatusOK)
-	assert.NoContains(t, city, "wrong city value", status)
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	require.Equal(t, "wrong city value", responseRecorder.Body.String())
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -41,9 +37,10 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
